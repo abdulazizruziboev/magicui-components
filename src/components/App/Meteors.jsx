7de3f98954f0c5b62a2b3b735c6847dc1a71f02c
abdulaziz <telegrampreimum4@gmail.com>
2026-02-05 14:22:33 +0500

    #

--- src/components/App/Meteors.jsx
+++ src/components/App/Meteors.jsx
@@ -2,7 +2,7 @@ import { Meteors } from "@/components/ui/meteors"
 
 export function MeteorDemo() {
   return (
-    <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden">
+    <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden pointer-events-none">
       <Meteors number={30} />
     </div>
   )
